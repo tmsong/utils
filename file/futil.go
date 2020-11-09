9275e1de4dd4851767d5370d9189a8b320338dff
songtianming <tm.song@huolala.cn>
2020-11-09 15:54:11 +0800

    add some pkg

---
+++ file/futil.go
@@ -0,0 +1,43 @@
+package file
+
+import (
+	"os"
+)
+
+// IsExist return true if path exist
+//
+// https://gist.github.com/mattes/d13e273314c3b3ade33f
+// if _, err := os.Stat("/path/to/whatever"); err == nil {
+// 	// path/to/whatever exists
+// }
+func IsExist(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+// IsNotExist return true if path not exist
+//
+// https://gist.github.com/mattes/d13e273314c3b3ade33f
+// if _, err := os.Stat("/path/to/whatever"); os.IsNotExist(err) {
+// 	// path/to/whatever does not exist
+// }
+func IsNotExist(path string) bool {
+	_, err := os.Stat(path)
+	return os.IsNotExist(err)
+}
+
+// IsDir return true if path exist and is directory
+func IsDir(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		return false
+	}
+
+	return fi.Mode().IsDir()
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/tmsong/utils
 
 go 1.15
 
-require github.com/stretchr/testify v1.6.1 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.2.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/tsaikd/KDGoLib v0.0.0-20191001134900-7f3cf518e07d
+)
